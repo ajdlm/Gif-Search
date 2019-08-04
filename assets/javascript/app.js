00643d08f57f8c9f2e71dfd16060e58df1d94ecd
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-08-04 11:51:33 -0500

    Added a font, made the heading that font, changed the CSS of the heading and buttons to make them translucent, among other things, added an input bar with similar CSS below the buttons for adding movies, made it so that it adds the movie you write in the input into the array/button div, made it so that all dynamically created buttons respond to an on-click event (tested with an alert)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,30 @@
 $(document).ready(function () {
-    var topics = ["Blade Runner", "The Matrix", "Timecrimes", "Dark City", "Planet of the Apes", "Twelve Monkeys", "Alien", "2001: A Space Odyssey", "District 9", "Snowpiercer", "Mad Max", "Children of Men", "Back to the Future", "Star Wars"];
-
-    console.log(topics.length);
+    var topics = ["Blade Runner", "The Matrix", "Timecrimes", "Dark City", "Planet of the Apes", "Twelve Monkeys", "Alien", "2001: A Space Odyssey", "District 9", "Snowpiercer", "Mad Max", "Children of Men", "Back to the Future", "Star Wars", "Star Trek", "Serenity", "E.T.", "Inception", "Metropolis", "The Fifth Element"];
 
     function createButtons() {
         for (var i = 0; i < topics.length; i++) {
             var newButton = $("<button>");
-            newButton.text(topics[i]).css("background", "teal").css("color", "white").css("border", "none").css("margin", ".5vw").css("border-radius", "10px");
+            newButton.text(topics[i]).addClass("movie-button").attr("data-name", topics[i]).css("background", "rgba(184, 183, 183, 0.3)").css("color", "white").css("border", "solid .2vh").css("margin", ".5vw").css("border-radius", "10px");
+
             $("#buttons-here").append(newButton);
         };
     };
 
     createButtons();
+
+    $("#submit-movie").on("click", function(event) {
+        newMovie = $("#add-movie").val().trim();
+
+        topics.push(newMovie);
+
+        $("#buttons-here").empty();
+
+        createButtons();
+    });
+
+    $("#buttons-here").on("click", ".movie-button", function(event) {
+        alert("Wow, you click button of " + $(this).attr("data-name") + "!");
+
+        
+    });
 });
